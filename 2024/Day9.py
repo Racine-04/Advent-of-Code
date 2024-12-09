a2b019789469e835e30423ee040a4612f8798376
Racine Kane <rkane082@uottawa.ca>
2024-12-09 02:09:27 -0500

    Day 9 part 2 (there's a bug somewhere)

--- 2024/Day9.py
+++ 2024/Day9.py
@@ -8,12 +8,12 @@ puzzle_day = '9'
 puzzle_input_url = f'https://adventofcode.com/{puzzle_year}/day/{puzzle_day}/input'
 
 req = requests.get(puzzle_input_url, cookies=cookies)
-diskMap = list("2333133121414131402")
+diskMap = list(req.text)
 
 id = 0
 idCount = {}
-space = {}
 rearrangedDiskMap = []
+
 for i in range(len(diskMap)):
     if(diskMap[i] == '\n'):
         continue
@@ -26,18 +26,43 @@ for i in range(len(diskMap)):
         for j in range(int(diskMap[i])):
             rearrangedDiskMap.append(".")
         
-        space[id - 1] = int(diskMap[i])
-
 left = 0
 right = len(rearrangedDiskMap) - 1
+moved = {-1}
 
 while(left < right):
-    if(rearrangedDiskMap[left] != "."):
-        left += 1
-    elif(rearrangedDiskMap[right] == "."):
+    if rearrangedDiskMap[right] == ".":
         right -= 1
+    elif rearrangedDiskMap[right] not in moved: 
+        groupSize = idCount[int(rearrangedDiskMap[right])]
+        groupStartIndex = right - groupSize + 1
+        
+        i = 0
+        space = 0
+        start = 0
+        while i < len(rearrangedDiskMap) and i < groupStartIndex:
+            if rearrangedDiskMap[i] == ".":
+                start = start or i
+                space += 1
+            else:
+                if(groupSize <= space):
+                    # Add
+                    for j in range(groupSize):
+                        rearrangedDiskMap[j + start] = rearrangedDiskMap[right]
+
+                    # Remove
+                    for j in range(groupSize):
+                        rearrangedDiskMap[j+groupStartIndex] = "."
+                    
+                    break
+                space = 0
+                start = 0
+            i += 1
+
+        moved.add(rearrangedDiskMap[right])
+        right = groupStartIndex - 1
     else:
-        rearrangedDiskMap[left], rearrangedDiskMap[right] = rearrangedDiskMap[right], rearrangedDiskMap[left]
+        right -= 1
 
 i = 0
 sum = 0
